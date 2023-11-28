b77754e51225cc09190b07888b792cd9c523347d
HEAVYxRAIN <heavyxrain@yandex.ru>
2023-11-28 19:24:10 +0300

    7. Балансир [#505083]

---
+++ src/test/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,20 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+
+    public static void split(
+            List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        while (source.hasNext()) {
+            for (ArrayList<Integer> element : nodes) {
+                element.add(source.next());
+                if (!source.hasNext()) {
+                    break;
+                }
+            }
+        }
+    }
+}
