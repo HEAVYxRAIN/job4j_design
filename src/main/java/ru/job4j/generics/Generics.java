f096339ff01fbb07a0af2db84fd4f9131baf8cda
HEAVYxRAIN <heavyxrain@yandex.ru>
2023-12-01 19:08:08 +0300

    1. Что такое обобщенные типы (generics) [#4952]

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -7,13 +7,10 @@ import java.util.List;
 public class Generics {
     public static void main(String[] args) {
         Generics generics = new Generics();
-        /*List<Animal> first = new ArrayList<>();*/
-        List<Predator> first = new ArrayList<>();
+        List<Animal> first = new ArrayList<>();
         List<Predator> second = new ArrayList<>();
-        /*List<Tiger> third = new ArrayList<>();*/
-        List<Predator> third = new ArrayList<>();
-        /*first.add(new Animal("Monkey", "Bananas", 20));*/
-        first.add(new Tiger("Monkey", "Bananas", 20));
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal("Monkey", "Bananas", 20));
         second.add(new Predator("Feline", "Meat", 15 - 30));
         third.add(new Tiger("King", "Gazelle", 30));
 
@@ -22,14 +19,16 @@ public class Generics {
         generics.printObject(third);
         System.out.println();
 
-        generics.printBoundedWildCard(first);
+        /*generics.printBoundedWildCard(first);*/
+        generics.printBoundedWildCard(second);
         generics.printBoundedWildCard(second);
         generics.printBoundedWildCard(third);
         System.out.println();
 
         generics.printLowerBoundedWildCard(first);
         generics.printLowerBoundedWildCard(second);
-        generics.printLowerBoundedWildCard(third);
+        /*generics.printLowerBoundedWildCard(third);*/
+        generics.printLowerBoundedWildCard(second);
     }
 
     /*public void printObject(List<Object> list) {
