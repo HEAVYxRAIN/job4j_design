fc6f93bc7d6ca3ff21a4791a180367d69b80e573
HEAVYxRAIN <heavyxrain@yandex.ru>
2024-04-06 14:36:21 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,62 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                if (!line.isBlank() && !line.startsWith("#")) {
+                    String[] keyAndValue = line.split("=", 2);
+                    if (keyAndValue.length != 2
+                            || keyAndValue[0].isEmpty()
+                            || keyAndValue[1].isEmpty()) {
+                        throw new IllegalArgumentException(""
+                                + "Нарушение шаблона key=value");
+                    }
+                    values.put(keyAndValue[0], keyAndValue[1]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при выводе данных из файла!");
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        if (!values.containsKey(key)) {
+            throw new NoSuchElementException("Отсутствует ключ " + key);
+        }
+        return this.values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            System.out.println("Ошибка при выводе данных из файла!");
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenViolationPattern() {
+        String path = "./data/violation_pattern.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Нарушение шаблона key=value");
+    }
+}
